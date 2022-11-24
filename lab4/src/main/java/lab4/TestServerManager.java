57309546f5e272ce7734105730c83c64cd70e5fa
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-11-24 11:01:09 +0100

    TestServerManager with a more "automatic" approach on messages to send to Server

--- lab4/src/main/java/lab4/TestServerManager.java
+++ lab4/src/main/java/lab4/TestServerManager.java
@@ -4,39 +4,50 @@ public class TestServerManager {
 
     private final static String IP = "127.0.0.1";
     private final static int SMTP_PORT = 25000;
+    private final static String CRLF = "\r\n";
 
     public static void main(String[] args) {
+        String[] msgFormat = {
+            "HELO Dump",
+            "MAIL FROM: <%s>",
+            "RCPT TO: <%s>",
+            "DATA",
+            "QUIT"
+        };
+        String sender = "boblebricoleur@gmail.com";
+        String[] recipients = {
+                "gne@gmail.com"
+        };
+        String msgBody = "Hey!" + CRLF + "Listen!";
         try {
             ServerManager server = new ServerManager(IP, SMTP_PORT);
             System.out.println("S: " + server.receive());
 
-            server.send("HELO Dump");
-            System.out.println("S: " + server.receive());
+            for (int i = 0; i < msgFormat.length; i++) {
+                switch (i) {
+                    case 1:
+                        server.send(String.format(msgFormat[i], sender));
+                        break;
+                    case 2:
+                        for (String recipient : recipients) {
+                            server.send(String.format(msgFormat[i], recipient));
+                        }
+                        break;
+                    case 3:
+                        server.send(msgFormat[i]);
+                        System.out.println("S: " + server.receive());
 
-            /* Sender */
-            server.send("MAIL FROM: <" + "boblebricoleur@gmail.com" + ">");
-            System.out.println("S: " + server.receive());
-
-            /* Receptionist */
-            server.send("RCPT TO: <" + "gne@gmail.com" + ">");
-            System.out.println("S: " + server.receive());
-
-            /* DATA */
-            server.send("DATA");
-            System.out.println("S: " + server.receive());
-
-            /* Mail's Body */
-            server.send("From: boblebricoleur@gmail.com");
-            server.send("To: gne@gmail.com");
-            server.send("Subject: Test");
-            server.send();
-            server.send("Hello world!");
-            server.send(".");
-            System.out.println("S: " + server.receive());
-
-            /* QUITTING */
-            server.send("QUIT");
-            System.out.println("S: " + server.receive());
+                        server.send("Subject: Test");
+                        server.send();
+                        server.send(msgBody);
+                        server.send(".");
+                        break;
+                    default:
+                        server.send(msgFormat[i]);
+                        break;
+                }
+                System.out.println("S: " + server.receive());
+            }
 
             server.close();
         } catch (Exception e) {
