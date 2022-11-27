b04733babe7959a39fedecba94d41ae2c21b0b18
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-11-27 17:29:59 +0100

    JSONMANAGER CLASS

---
+++ lab4/src/main/java/lab4/JSONManager.java
@@ -0,0 +1,34 @@
+package lab4;
+
+import java.io.FileReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class JSONManager {
+
+    public JSONArray readFromFile(String filename) {
+        JSONParser jsonParser = new JSONParser();
+        JSONArray jsonArray = null;
+
+        try
+        {   /* Open file through FileReader */
+            FileReader fr = new FileReader(filename);
+
+            /* Read JSON file as Object */
+            Object obj = jsonParser.parse(fr);
+
+            /* Object parsed as jsonArray */
+            jsonArray = (JSONArray) obj;
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+        return jsonArray;
+    }
+
+    public static JSONObject parseAs(JSONObject jsonObj, String type) {
+         return (JSONObject) jsonObj.get(type);
+    }
+}
